21514c0467595b3eb48dc245d8a13c2563aaec14
Xiaoke <feixiaoke@gmail.com>
2017-05-12 16:51:40 -0400

    doc: add comments and explanations in WAVE class

--- wave.py
+++ wave.py
@@ -4,25 +4,73 @@ from sklearn.tree import DecisionTreeClassifier
 class WAVE(object):
 	
 	def __init__(self, ensemble_size, base_ensemble="rf", min_samples_split_cerp=5):
+		"""
+		Args:
+			ensemble_size: int, number of base classifiers
+			base_ensemble: option of base ensemble type, one of the following 3:
+						   "rf":      Random Forest
+						   "bagging": Bagging
+						   "cerp":    CERP
+			min_samples_split_cerp: int, >= 2, default = 5,
+			               The minimum number of samples required to split an internal node	for trees in CERP
+						   This argument controlls the complexity of base trees in CERP
+		"""
 		self.ensemble_size = ensemble_size
 		self.base_ensemble = base_ensemble
-		self.weights = None
-		self.base_classifiers = []
-		self.subfeatures_list = None
 		self.min_samples_split_cerp = min_samples_split_cerp
+		
+		# weight vector for base classifiers
+		self.weights = None
+		
+		# list for storing base classifiers
+		self.base_classifiers = []
+		
+		# list of subfeatures's column indexes of CERP
+		# each element is a 1-d numpy array consisting of column indexes of training features
+		# this list is for predicting use of CERP
+		self.subfeatures_list = None
+		
+		# list of class labels
 		self.class_labels = None
 		
 	def fit(self, train_X, train_y):
+		"""
+		Fit the WAVE Ensemble consisting of base classifiers and corresponding weights
+		Args:
+		    train_X: 2-d numpy array, size=(n, k)
+			train_Y: 1-d numpy array, size=(n, )
+		
+		Return: None
+		Update: self.class_labels
+		        self.base_classifiers
+		        self.weights 
+		"""
+		# compute class labels 
 		self.class_labels = np.unique(train_y)
+		
+		# fit base classifiers of the ensemble using helper function
 		self.fit_base_classifiers(train_X, train_y)
+		
+		# compute weight vector using helper function
 		self.compute_weights(train_X, train_y)
 		
 		
 	
 	def fit_base_classifiers(self, train_X, train_y):
+		"""
+		Fit Base Classifiers, a helper function used in fit() method
+		Args:
+		    train_X: 2-d numpy array, size=(n, k)
+			train_Y: 1-d numpy array, size=(n, )
+			
+		Return: None
+		Update: self.base_classifiers
+		"""
+		# if the base ensemble is CERP, call fit_cerp helper function
 		if self.base_ensemble == "cerp":
 			self.fit_cerp(train_X, train_y, self.min_samples_split_cerp)
 		
+		# if base ensemble is Random Forest or Bagging
 		else:
 			# set the option of number of features at each splitting node
 			if self.base_ensemble == "rf":
@@ -34,7 +82,6 @@ class WAVE(object):
 		
 		    # train base classifiers
 			for i in range(self.ensemble_size):
-				
 				# bootstrap sampling from training set
 				idxes = np.random.choice(np.arange(len(train_X)), size=len(train_X), replace=True)
 				bootstrap_X = train_X[idxes]
@@ -43,9 +90,21 @@ class WAVE(object):
 				# train base tree
 				tree = DecisionTreeClassifier(max_features=max_features)
 				tree.fit(bootstrap_X, bootstrap_y)
+				
+				# append the base tree to the list of base classifiers
 				self.base_classifiers.append(tree)
 	
 	def fit_cerp(self, train_X, train_y, min_samples_split):
+		"""
+		Fit CERP ensemble, a helper function used in fit_base_classifiers() method
+		Args:
+		    train_X: 2-d numpy array, size=(n, k)
+			train_Y: 1-d numpy array, size=(n, )
+			min_samples_split: The minimum number of samples required to split an internal node	for trees in CERP
+						       This argument controlls the complexity of base trees in CERP
+		Return: None
+		Update: self.base_classifiers
+		"""
 		
 		# number of features
 		n_cols = train_X.shape[1]
@@ -73,21 +132,25 @@ class WAVE(object):
 		
 		# train base classifiers
 		for i in range(self.ensemble_size):
-			# extract sub features' index from the list
+			# extract sub features' indexes from the list
 			sub_features = self.subfeatures_list[i]
 			
 			# extract sub training set by subspace of the features
 			sub_X = train_X[:, sub_features]
-			#print (sub_X)
 				
 			tree = DecisionTreeClassifier(min_samples_split = min_samples_split)
 			tree.fit(sub_X, train_y)
-				
+			
+			# update self.base_classifiers by appending the base tree
 			self.base_classifiers.append(tree)
 			
 			
 	
 	def compute_weights(self, train_X, train_y):
+		"""
+		Comput Weights for Base Classifiers
+		
+		"""
 		# performance matrix:
 		X = self.performance_matrix(train_X, train_y)
 		
